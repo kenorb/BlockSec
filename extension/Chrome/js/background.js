95226f9a3125ee9fd02fd04c2fa3e5db08fda1f2
kenorb <kenorb@users.noreply.github.com>
2018-09-22 22:12:55 +0100

    Adds code detecting SSL certificate

--- extension/Chrome/js/background.js
+++ extension/Chrome/js/background.js
@@ -1,3 +1,31 @@
 /**
  * @file The entry point for the web extension.
  */
+
+var log = console.log.bind(console)
+
+log('\n\nTLS browser extension loaded.')
+
+// @refs: https://developer.chrome.com/extensions/match_patterns
+var ALL_SITES = { urls: ['<all_urls>'] }
+
+// Mozilla doesn't use tlsInfo in extraInfoSpec.
+var extraInfoSpec = ['blocking'];
+
+// @refs: https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/webRequest/onHeadersReceived
+browser.webRequest.onHeadersReceived.addListener(async function(details){
+    log('\n\nGot a request for ${details.url} with ID ${details.requestId}.')
+
+    // Yeah this is a String, even though the content is a Number
+    var requestId = details.requestId
+
+    var securityInfo = await browser.webRequest.getSecurityInfo(requestId, {
+        certificateChain: true,
+        rawDER: false
+    });
+
+    log('securityInfo: ${JSON.stringify(securityInfo, null, 2)}')
+
+}, ALL_SITES, extraInfoSpec)
+
+log('Added listener.')
