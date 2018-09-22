fc30be95f4911cba10e146ba4a97f1a54b11b061
kenorb <kenorb@users.noreply.github.com>
2018-09-22 23:45:22 +0100

    Updates background and manifest files

--- extension/js/background.js
+++ extension/js/background.js
@@ -4,8 +4,6 @@
 
 var log = console.log.bind(console)
 
-log('\n\nTLS browser extension loaded.')
-
 // @refs: https://developer.chrome.com/extensions/match_patterns
 var ALL_SITES = { urls: ['<all_urls>'] }
 
@@ -13,19 +11,21 @@ var ALL_SITES = { urls: ['<all_urls>'] }
 var extraInfoSpec = ['blocking'];
 
 // @refs: https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/webRequest/onHeadersReceived
-browser.webRequest.onHeadersReceived.addListener(async function(details){
-    log('\n\nGot a request for ${details.url} with ID ${details.requestId}.')
+if (typeof browser !== "undefined") {
+    log('TLS browser extension loaded.')
+    browser.webRequest.onHeadersReceived.addListener(async function(details){
+        log('Got a request for ${details.url} with ID ${details.requestId}.')
 
-    // Yeah this is a String, even though the content is a Number
-    var requestId = details.requestId
+        // Yeah this is a String, even though the content is a Number
+        var requestId = details.requestId
 
-    var securityInfo = await browser.webRequest.getSecurityInfo(requestId, {
-        certificateChain: true,
-        rawDER: false
-    });
+        var securityInfo = await browser.webRequest.getSecurityInfo(requestId, {
+            certificateChain: true,
+            rawDER: false
+        });
 
-    log('securityInfo: ${JSON.stringify(securityInfo, null, 2)}')
+        log('securityInfo: ${JSON.stringify(securityInfo, null, 2)}')
 
-}, ALL_SITES, extraInfoSpec)
-
-log('Added listener.')
+    }, ALL_SITES, extraInfoSpec)
+    console.log('Added listener.')
+}
